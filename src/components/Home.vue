af2a13b5935d0302ce44a2e6097aececc48edf83
penny <penny_kuo@mail.chungyo.net>
2020-01-12 14:25:25 +0800

    default settings

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,35 +10,12 @@ export default {
 		
 	},
 	computed: {
-		...mapState('Chess', [
-			'randomArray',
-		]),
-		...mapState([
-			'defaultTeams',
-		]),
+		
 	},
 	async mounted() {
-		await this.getTeams();
+		
 	},
 	methods: {
-		async click() {
-			await this.axios.post(
-				'https://meetupdimashirokovv1.p.rapidapi.com/getCities',
-				{ headers: {
-					'x-rapidapi-host': 'MeetupdimashirokovV1.p.rapidapi.com',
-					'x-rapidapi-key': 'c49dfc3f3cmshc1c1efbdc793d17p17b4c0jsnb30d722e5885',
-					'content-type': 'application/x-www-form-urlencoded',
-				},
-				body: {
-					accessToken: {},
-				} },
-			).then(responese=>{
-				console.log(responese.data);
-			});
-		},
-		...mapActions('Chess', [
-			'getTeams',
-		]),
 		
 	},
 	
